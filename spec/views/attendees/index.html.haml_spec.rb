0398ffd00eeba38c5abfce28d32400a27d53d988
ac033376 <aaron.cowdrey@cerner.com>
2016-11-19 06:08:09 +0000

    fixed specs from scaffold and missing gem

--- spec/views/attendees/index.html.haml_spec.rb
+++ spec/views/attendees/index.html.haml_spec.rb
@@ -4,37 +4,37 @@ RSpec.describe "attendees/index", type: :view do
   before(:each) do
     assign(:attendees, [
       Attendee.create!(
-        :email => "MyText",
-        :first_name => "MyText",
-        :last_name => "MyText",
-        :all_attendants => "MyText",
+        :email => "MyText0",
+        :first_name => "MyText1",
+        :last_name => "MyText2",
+        :all_attendants => "MyText3",
         :party_count => 2,
         :rsvp => false,
-        :is_attending => false,
-        :number_attending => 3
+        :is_attending => true,
+        :number_attending => 1
       ),
       Attendee.create!(
-        :email => "MyText",
-        :first_name => "MyText",
-        :last_name => "MyText",
-        :all_attendants => "MyText",
+        :email => "MyText0",
+        :first_name => "MyText1",
+        :last_name => "MyText2",
+        :all_attendants => "MyText3",
         :party_count => 2,
         :rsvp => false,
-        :is_attending => false,
-        :number_attending => 3
+        :is_attending => true,
+        :number_attending => 1
       )
     ])
   end
 
   it "renders a list of attendees" do
     render
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText0".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText1".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText2".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText3".to_s, :count => 2
     assert_select "tr>td", :text => 2.to_s, :count => 2
     assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
+    assert_select "tr>td", :text => true.to_s, :count => 2
+    assert_select "tr>td", :text => 1.to_s, :count => 2
   end
 end
--- spec/views/attendees/show.html.haml_spec.rb
+++ spec/views/attendees/show.html.haml_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe "attendees/show", type: :view do
   before(:each) do
     @attendee = assign(:attendee, Attendee.create!(
-      :email => "MyText",
-      :first_name => "MyText",
-      :last_name => "MyText",
-      :all_attendants => "MyText",
+      :email => "MyText0",
+      :first_name => "MyText1",
+      :last_name => "MyText2",
+      :all_attendants => "MyText3",
       :party_count => 2,
       :rsvp => false,
-      :is_attending => false,
-      :number_attending => 3
+      :is_attending => true,
+      :number_attending => 1
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/MyText0/)
+    expect(rendered).to match(/MyText1/)
+    expect(rendered).to match(/MyText2/)
+    expect(rendered).to match(/MyText3/)
     expect(rendered).to match(/2/)
     expect(rendered).to match(/false/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/3/)
+    expect(rendered).to match(/true/)
+    expect(rendered).to match(/1/)
   end
 end
